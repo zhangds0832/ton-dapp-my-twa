f7358bc60268867259657beb5c75397b72ac25e2
zhangds0832 <zhangds0832@gmail.com>
2024-08-09 15:10:14 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: "/ton-dapp-my-twa/",
+  base: "./",
   server: {
     host: "0.0.0.0"
   }
